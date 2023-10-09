2c3f45bd5f2f843c95481f92e567c8db770a9940
S.K.Obidul kadir <rezwankadirobaidul@gmail.com>
2023-10-09 22:43:31 +0600

    Locator Basic

--- src/test/java/Web/Automation/Locator.java
+++ src/test/java/Web/Automation/Locator.java
@@ -2,6 +2,7 @@ package Web.Automation;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class Locator extends BaseDriver{
@@ -13,6 +14,10 @@ public class Locator extends BaseDriver{
 		driver.manage().window().maximize();
 		driver.get(url);
 		Thread.sleep(5000);
+		
+		String WebSiteTitle = driver.getTitle();
+		System.out.println("Website title is : "+WebSiteTitle);
+		
 		/*
 		 CssSelector.
 		1. Element Selector
@@ -28,17 +33,54 @@ public class Locator extends BaseDriver{
 		 * 
 		 * 
 		 */
+		
+		
 		WebElement register = driver.findElement(By.cssSelector(".ico-register"));
 		register.click();
 		Thread.sleep(5000);
 		
+		// print the title using cssSelector. tag
+		WebElement title = driver.findElement(By.cssSelector("h1"));
+		System.out.println("Page title: "+title.getText());
+		
+		//xpath - //tagname[contains(text(), 'String')]
+		WebElement SubTitle = driver.findElement(By.xpath("//strong[contains(text(), 'Your Personal Details')]"));
+		System.out.println("SubTitle : "+SubTitle.getText());
+		
 		//ID
 		WebElement FirstName = driver.findElement(By.id("FirstName"));
 		FirstName.click();
-		FirstName.sendKeys("Rikta");
+		FirstName.sendKeys("Kadir");
+		
+		//cssSelector
+		WebElement LastName = driver.findElement(By.xpath("//input[@name ='LastName']"));
+		LastName.click();
+		LastName.sendKeys("Ahmed");
+		Thread.sleep(3000);
+		
+		//dropdown
+		//select a dob
+		WebElement DOB = driver.findElement(By.xpath("//select[@name='DateOfBirthDay']"));
+		Select selectDOB = new Select(DOB);
+		selectDOB.selectByIndex(5);
+		Thread.sleep(3000);
+		
+//		select.selectByVisibleText("10");
+//		Thread.sleep(1000);
 		
 		
+		//Months
+		WebElement months = driver.findElement(By.xpath("//*[@name='DateOfBirthMonth']"));
+		Select Month = new Select(months);
+		Month.selectByVisibleText("April");
+		Thread.sleep(2000);
 		
+		// year
+		//cssSelector - [attribute ='value']
+		WebElement year = driver.findElement(By.cssSelector("[name='DateOfBirthYear']"));
+		Select SelectYear = new Select(year);
+		SelectYear.selectByIndex(10);
+		Thread.sleep(2000);
 		
 		
 	}
