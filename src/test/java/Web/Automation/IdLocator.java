cee682b2f1e5a699d8a670a64ed599430b3c3da3
S.K.Obidul kadir <rezwankadirobaidul@gmail.com>
2023-10-10 20:10:41 +0600

    Id Selector

---
+++ src/test/java/Web/Automation/IdLocator.java
@@ -0,0 +1,43 @@
+package Web.Automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class IdLocator extends BaseDriver{
+	
+	String url = "https://demo.nopcommerce.com/";
+	
+	@Test
+	public void ExceuteUrl() throws InterruptedException {
+		driver.manage().window().maximize();
+		driver.get(url);
+		Thread.sleep(3000);
+		
+		/*
+		 * Id Selector
+		 * Ex: element = driver.find_element_by_id("yourIDName")
+		 * 
+		 */
+		//ID
+		
+		Actions action = new Actions(driver);
+		WebElement ShoppingCart = driver.findElement(By.id("topcartlink"));
+		
+		action.moveToElement(ShoppingCart).perform();
+		
+		System.out.println("Tag name:"+ShoppingCart.getTagName()+" Inner text: "+ShoppingCart.getText());
+		
+		Thread.sleep(3000);
+		
+		// cssSelector Class
+		WebElement ShoppingCartHoverText = driver.findElement(By.cssSelector(".count"));
+		System.out.println("Hover element text : "+ShoppingCartHoverText.getText());
+		
+		
+		
+		
+	}
+
+}
